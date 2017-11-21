fb42f7fbd70de70948d169b09e3b262afb85d165
Zarko Popovski <zarko.popovski@gmail.com>
2017-11-21 22:45:07 +0100

    Simple ini configuration

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	//"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/knq/ini"
 )
 
 type GFServCore struct {
@@ -37,8 +39,19 @@ func CreateNewRouter(handlers *GFServCore) *httprouter.Router {
 }
 
 func main() {
+	fileCfg, err := ini.LoadFile("config.cfg")
+	if err != nil {
+		log.Fatal("Error with service configuration %s", err)
+	}
+
+	port := fileCfg.GetKey("service-1.port")
+
+	if port == "" {
+		log.Fatal("Error with port number configuration")
+	}
+
 	gfService := CreateGFService()
 	router := CreateNewRouter(gfService)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
